4f0f3f26ae47e96bf83b1af873358d0dfada8e62
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-11-26 12:36:47 +0100

    :green_heart: [https://github.com/maykinmedia/open-api-framework/issues/66] update old tests

--- src/referentielijsten/api/tests/test_tabellen.py
+++ src/referentielijsten/api/tests/test_tabellen.py
@@ -1,39 +1,38 @@
 from django.test import override_settings
+from django.urls import reverse_lazy
 
 from freezegun import freeze_time
 from rest_framework import status
 from rest_framework.test import APITestCase
-from vng_api_common.tests import reverse
 
-from ..models import Tabel
 from .factories import TabelFactory
 
 
 @override_settings(USE_TZ=False)
 class TabellenApiTests(APITestCase):
+    url = reverse_lazy("tabel-list", kwargs={"version": 1})
+
     def test_tabellen_with_no_query_params(self):
-        url = reverse(Tabel)
         TabelFactory.create_batch(3)
 
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()
         self.assertEqual(response_data["count"], 3)
 
     def test_tabellen_with_code(self):
-        url = reverse(Tabel)
         TabelFactory.create(code="123", naam="first tabel")
         TabelFactory.create(code="456", naam="second tabel")
         TabelFactory.create(code="789", naam="third tabel")
 
-        response = self.client.get(url, {"code": "456"})
+        response = self.client.get(self.url, {"code": "456"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()["results"]
 
         self.assertEqual(response_data[0]["code"], "456")
         self.assertEqual(response_data[0]["naam"], "second tabel")
 
-        response = self.client.get(url, {"code": "123456789"})
+        response = self.client.get(self.url, {"code": "123456789"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()
 
@@ -41,7 +40,6 @@ class TabellenApiTests(APITestCase):
 
     @freeze_time("2024-01-01 00:00:00")
     def test_tabellen_is_geldig(self):
-        url = reverse(Tabel)
         TabelFactory.create(
             code="1", naam="geldig", einddatum_geldigheid="2025-01-01 00:00:00"
         )
@@ -49,7 +47,7 @@ class TabellenApiTests(APITestCase):
             code="2", naam="ongeldig", einddatum_geldigheid="2023-01-01 00:00:00"
         )
 
-        response = self.client.get(url, {"isGeldig": "true"})
+        response = self.client.get(self.url, {"isGeldig": "true"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()
 
@@ -59,7 +57,6 @@ class TabellenApiTests(APITestCase):
 
     @freeze_time("2024-01-01 00:00:00")
     def test_tabellen_is_not_geldig(self):
-        url = reverse(Tabel)
         TabelFactory.create(
             code="1", naam="geldig", einddatum_geldigheid="2025-01-01 00:00:00"
         )
@@ -67,7 +64,7 @@ class TabellenApiTests(APITestCase):
             code="2", naam="ongeldig", einddatum_geldigheid="2023-01-01 00:00:00"
         )
 
-        response = self.client.get(url, {"isGeldig": "false"})
+        response = self.client.get(self.url, {"isGeldig": "false"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()
 
