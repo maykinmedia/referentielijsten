4999ef867ecbde9cd536db63b07f504e50b695f3
Tim de Beer <tim@maykinmedia.nl>
2025-05-16 09:12:27 +0200

    :sparkles: [#70] made confirm import hidden
    	modified:   src/referentielijsten/api/forms.py

--- src/referentielijsten/api/forms.py
+++ src/referentielijsten/api/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from import_export.forms import ConfirmImportForm, ImportForm
 
@@ -10,12 +11,16 @@ class ItemImportForm(ImportForm):
         queryset=Tabel.objects.all(),
         label="Tabel",
         required=False,
-        help_text="Selecteer een tabel om toe te passen op alle geïmporteerde items, "
-        "tenzij de tabel al is aangegeven in het bestand.",
+        help_text=_(
+            "Selecteer een tabel voor alle geïmporteerde items. "
+            "Als je hier een tabel selecteert, wordt een eventuele 'table_id' in het bestand genegeerd."
+        ),
     )
 
 
 class ItemConfirmImportForm(ConfirmImportForm):
     selected_tabel = forms.ModelChoiceField(
-        queryset=Tabel.objects.all(), label="Tabel", required=False
+        queryset=Tabel.objects.all(),
+        required=False,
+        widget=forms.HiddenInput(),
     )
