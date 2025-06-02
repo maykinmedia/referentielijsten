2d476e2598dd4e576b598112a4a58a6a1e51fef5
Tim de Beer <tim@maykinmedia.nl>
2025-06-02 15:02:13 +0200

    :wrench: [#88] Configure structlog settings

--- src/referentielijsten/conf/dev.py
+++ src/referentielijsten/conf/dev.py
@@ -17,6 +17,7 @@ os.environ.setdefault("DB_PASSWORD", "referentielijsten")
 os.environ.setdefault("RELEASE", "dev")
 os.environ.setdefault("ENVIRONMENT", "development")
 os.environ.setdefault("DISABLE_2FA", "True")
+os.environ.setdefault("LOG_FORMAT_CONSOLE", "plain_console")
 
 from .base import *  # noqa isort:skip
 
@@ -33,29 +34,29 @@ LOGGING["loggers"].update(
         "referentielijsten": {
             "handlers": ["console"],
             "level": "DEBUG",
-            "propagate": True,
+            "propagate": False,
         },
         "django": {
             "handlers": ["console"],
             "level": "DEBUG",
-            "propagate": True,
+            "propagate": False,
         },
         "django.db.backends": {
-            "handlers": ["django"],
+            "handlers": ["json_file"],
             "level": "DEBUG",
             "propagate": False,
         },
         "performance": {
             "handlers": ["console"],
             "level": "INFO",
-            "propagate": True,
+            "propagate": False,
         },
         #
         # See: https://code.djangoproject.com/ticket/30554
         # Autoreload logs excessively, turn it down a bit.
         #
         "django.utils.autoreload": {
-            "handlers": ["django"],
+            "handlers": ["console"],
             "level": "INFO",
             "propagate": False,
         },
