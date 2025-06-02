3c634ccd442466bf3238b2093a2dd51b7198d83e
Tim de Beer <tim@maykinmedia.nl>
2025-06-02 15:02:13 +0200

    :wrench: [#88] Configure structlog settings

--- src/referentielijsten/conf/docker.py
+++ src/referentielijsten/conf/docker.py
@@ -8,6 +8,7 @@ os.environ.setdefault("DB_CONN_MAX_AGE", "60")
 
 os.environ.setdefault("ENVIRONMENT", "docker")
 os.environ.setdefault("LOG_STDOUT", "yes")
+os.environ.setdefault("LOG_FORMAT_CONSOLE", "json")
 
 # # Strongly suggested to not use this, but explicitly list the allowed hosts. It is
 # used to verify if a redirect is safe or not (open redirect vulnerabilities etc.)
